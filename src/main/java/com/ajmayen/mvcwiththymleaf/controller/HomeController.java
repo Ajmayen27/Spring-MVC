c557fb5cca6855ba56622477bfb0a6335e3af289
Ajmayen Fayek <fayek22205341186@diu.edu.bd>
2025-03-14 03:00:41 +0600

    Post Redirect Get(PRG) Pattern

--- src/main/java/com/ajmayen/mvcwiththymleaf/controller/HomeController.java
+++ src/main/java/com/ajmayen/mvcwiththymleaf/controller/HomeController.java
@@ -1,12 +1,20 @@
 package com.ajmayen.mvcwiththymleaf.controller;
 
+import jakarta.annotation.Nullable;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
+
+
+
+
 
 
     @RequestMapping("/home")
@@ -17,15 +25,24 @@ public class HomeController {
     }
 
 
-    @RequestMapping("/user-details")
-    public ModelAndView getAnotherHomePage(ModelAndView modelAndView, @RequestParam String name,@RequestParam int age,@RequestParam String mail){
-       modelAndView.setViewName("UserDetails");
-       modelAndView.addObject("name",name);
-       modelAndView.addObject("age",age);
-       modelAndView.addObject("mail",mail);
+    @PostMapping("/user-details")
+    public ModelAndView postUserData(ModelAndView modelAndView, @RequestParam String name, @RequestParam @Nullable Integer age, @RequestParam String mail){
+         modelAndView.setViewName("redirect:/user-details");
+//       modelAndView.addObject("name",name);
+//       modelAndView.addObject("age",age);
+//       modelAndView.addObject("mail",mail);
 
        return modelAndView;
+
     }
+
+    @GetMapping("/user-details")
+    public ModelAndView viewUserData(ModelAndView modelAndView){
+         modelAndView.setViewName("UserDetails");
+         return modelAndView;
+    }
+
+
 
 
 
